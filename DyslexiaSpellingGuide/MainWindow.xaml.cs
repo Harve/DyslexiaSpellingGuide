4c1f6aecdd2e253949f278951f174a2a2526430c
Harve <harvey290@hotmail.co.uk>
2020-11-22 17:01:09 +0000

    Prototype using System.Speech.Recognition

---
+++ DyslexiaSpellingGuide/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Recognition;
+
+namespace DyslexiaSpellingGuide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static SpeechRecognitionEngine engine;
+        public MainWindow()
+        {
+            engine = new SpeechRecognitionEngine();
+            engine.SetInputToDefaultAudioDevice();
+
+            engine.LoadGrammar(new DictationGrammar());
+
+            engine.RecognizeAsync(RecognizeMode.Multiple);
+
+            engine.SpeechRecognized += addWord;
+
+            InitializeComponent();
+            
+        }
+
+        public void addWord(object sender, SpeechRecognizedEventArgs result)
+        {
+            wordTextBox.Text = (result.Result.Text);
+        }
+    }
+}
